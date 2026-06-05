49fe2d4551e92cb666c3f141049d24758bd4d40f
szerming <szerming@gmail.com>
2026-06-05 20:26:49 +0100

    add more utils

--- src/agentic_researcher/utils/file_utils.py
+++ src/agentic_researcher/utils/file_utils.py
@@ -5,15 +5,30 @@ from pydantic import BaseModel
 
 class FileUtils:
     @staticmethod
+    def get_project_root() -> Path:
+        return Path(__file__).parent.parent.parent.parent
+
+    @staticmethod
     def get_outputs_path() -> Path:
         """
         Returns the path to the outputs directory.
         """
-        project_root = Path(__file__).parent.parent.parent.parent
+        project_root = FileUtils.get_project_root()
         outputs_dir = project_root / "outputs"
         outputs_dir.mkdir(exist_ok=True)
         return outputs_dir
 
+    @staticmethod
+    def get_resource_path() -> Path:
+        """
+        Returns the path to the outputs directory.
+        """
+        project_root = FileUtils.get_project_root()
+        resources_dir = project_root / "resources"
+        resources_dir.mkdir(exist_ok=True)
+        return resources_dir
+
+    @staticmethod
     def write_temporary_markdown_file(
         content: str | list[str] | BaseModel, filename: str = "temp.txt"
     ) -> Path:
@@ -29,5 +44,6 @@ class FileUtils:
             f.write(content)
         return filepath
 
+    @staticmethod
     def get_timestamp() -> str:
-        return datetime.now().strftime("%Y%m%d_%H%M%S")
+        return datetime.now().strftime("%Y%m%d_%H%M%S_%f")
